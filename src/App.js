de158f198b5b4b3e27164cf9afda3f60b478c094
Meiviezhi <meiviezhipadmanaban94@gmail.com>
2026-08-17 22:56:57 +0530

    Updated capstone application UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>DevOps Capstone Project</h1>
+
+        <p>Created by Meiviezhi P</p>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CI/CD using Jenkins, Docker, Kubernetes and AWS EKS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          🚀 Successfully deployed through Jenkins Pipeline
+        </p>
       </header>
     </div>
   );
